bcb5cf435651a78da5c0cf3dfef99314d485384e
Leo Hetsch <leo.hetsch@gmail.com>
2013-04-16 14:34:49 +0200

    added choice field type

---
+++ src/Forms/Field/ChoiceField.php
@@ -0,0 +1,41 @@
+<?php
+namespace Forms\Field;
+
+use Forms\Field\Field;
+
+class ChoiceField extends Field
+{
+    
+    protected $choices = array();
+    
+    public function addChoice($value, $label)
+    {
+        $this->choices[$value] = $label;
+        
+        return $this;
+    }
+    
+    public function getBody()
+    {
+        $id     = $this->getId() ? $this->getId() : $this->getName();
+        $name   = $this->getName();
+        $value  = $this->getValue() ? 'checked' : '';
+        $label  = $this->getLabel();
+        
+        $required = $this->hasRule('not_empty') ? 'required' : '';
+        
+        $body = "$label ";
+        foreach($this->choices as $opt_value => $opt_label) {
+            $body .= "<input $required type=\"radio\" name=\"$name\" value=\"$opt_value\" id=\"{$name}_{$opt_value}\" > <label for=\"{$name}_{$opt_value}\" >$opt_label</label>";
+        }
+        
+        return $body;
+    }
+    
+    
+
+    public function getFieldName()
+    {
+        return 'choice';
+    }
+}

de356d6b05414183b1c534ee31be49c9179f4e3d
Leo Hetsch <leo.hetsch@gmail.com>
2013-04-18 11:09:11 +0200

    added ignoreAttribute method

--- src/Forms/Field/ObjectField.php
+++ src/Forms/Field/ObjectField.php
@@ -4,10 +4,23 @@ namespace Forms\Field;
 use Forms\Field\Field;
 use Forms\Field\TextField;
 
+/**
+ * The ObjectField builds a subform with one field for each public attribute of the given object
+ * 
+ * @author Leo Hetsch 
+ */
 class ObjectField extends Field
 {
+    /**
+     * @var array
+     */
     protected $fields;
     
+    /**
+     * Constructor
+     * 
+     * @see Forms\Field\Field::__construct
+     */
     public function __construct($name, $label = null, $id = null, $value = null, array $builder_configuration = array())
     {
         if ( $value && !is_object($value) ) {
@@ -24,6 +37,11 @@ class ObjectField extends Field
         
     }
     
+    /**
+     * Override setValue() method to check if the value is an object.
+     * 
+     * @see Forms\Field\Field::setValue 
+     */
     public function setValue($value)
     {
         if ( $value && !is_object($value) ) {
@@ -39,21 +57,47 @@ class ObjectField extends Field
         }
     }
     
+    /**
+     * Add a field
+     * 
+     * @param string $name
+     * @param string $label
+     * @param string $id
+     * @param string $value
+     * @param array $builder_configuration 
+     */
     public function addField($name, $label, $id, $value, $builder_configuration = array())
     {
         $this->fields[$name] = new TextField($name, $label, $id, $value, $builder_configuration);
+        
+        return $this;
     }
     
+    /**
+     * @param string $name
+     * @return Boolean 
+     */
     public function hasField($name)
     {
         return array_key_exists($name, $this->fields);
     }
     
+    /**
+     * @param string $name
+     * @return Forms\Field\Field 
+     */
     public function getField($name)
     {
         return $this->fields[$name];
     }
     
+    /**
+     * Set the label for an attribute field
+     * 
+     * @param string $name the attribute name
+     * @param string $label the label
+     * @return \Forms\Field\ObjectField 
+     */
     public function setAttributeLabel($name, $label)
     {
         if ($this->hasField($name)) {
@@ -63,6 +107,13 @@ class ObjectField extends Field
         return $this;
     }
     
+    /**
+     * Set the rules for an attribute field
+     * 
+     * @param string $name the attribute name
+     * @param array $rules
+     * @return \Forms\Field\ObjectField 
+     */
     public function setAttributeRules($name, array $rules)
     {
         if ($this->hasField($name)) {
@@ -72,6 +123,35 @@ class ObjectField extends Field
         return $this;
     }
     
+    /**
+     * Tells the object field to ignore one attribute of the given object
+     * @param string $attribute 
+     */
+    public function ignoreAttribute($attribute)
+    {
+        if ($this->hasField($attribute)) {
+            unset($this->fields[$attribute]);
+        }
+    }
+    
+    /**
+     * Clone of ignoreAttribute() but for an array of attributes
+     * @see ignoreAttribute
+     * @param array $attributes
+     */
+    public function ignoreAttributes(array $attributes)
+    {
+        foreach ($attributes as $attribute) {
+            if ($this->hasField($attribute)) {
+                unset($this->fields[$attribute]);
+            }
+        }
+    }
+    
+    /**
+     * Get the field body with all the attribute fields
+     * @return string 
+     */
     public function getBody()
     {
         if (empty($this->fields)) return;
@@ -85,6 +165,10 @@ class ObjectField extends Field
         return $body;
     }
     
+    /**
+     * Get the field type name
+     * @return string 
+     */
     public function getFieldName()
     {
         return 'object';
